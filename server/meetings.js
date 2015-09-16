e0511555fe53be2b92ee07c98f40f5763c6e4a4f
root <marcin@bulynko.pl>
2015-09-16 14:02:30 +0400

    First commit

--- server/meetings.js
+++ server/meetings.js
@@ -1,6 +1,25 @@
 
-Meteor.publish("meetings", function (options, searchString) {
+//Meteor.publish("meetings", function (options, searchString) {
+//return Meetings.find({});
+//});
 
-return Meetings.find({});
 
+
+//Meteor.publish("meetings", function (options,searchString) {
+
+Meteor.publish("meetings", function () {
+
+
+  return Meetings.find({
+     $or:[
+      {$and:[
+        {"public": true},
+        {"public": {$exists: true}}
+      ]},
+      {$and:[
+        {uid: this.userId},
+        {uid: {$exists: true}},
+      ]}
+    ]} );
 });
+
